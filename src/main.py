99619e6b27f46fc7d62c920eb629e88f2353775a
MONTOYA COLOMA JOSE <jomontoyaco@uide.edu.ec>
2026-07-30 19:17:23 -0500

    taller9: sincorinizacioninicial

--- src/main.py
+++ src/main.py
@@ -1,9 +1,1 @@
-# Punto de entrada del programa
-# Reemplaza este archivo con tu código (o usa el lenguaje que corresponda al curso)
-
-def main():
-    print("Hola, este es el proyecto de [tu nombre]")
-
-
-if __name__ == "__main__":
-    main()
+print ("Hola")
